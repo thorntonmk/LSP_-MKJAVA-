d2905ad7386ef79aaf11b0f7693aeac801f418ff
Morgan Thornton <morgan.thornton@bison.howard.edu>
2023-01-25 21:25:39 -0500

    Update Driver.java

--- object oriented programming/src/org/howard/edu/lsp/assignment2/Driver.java
+++ object oriented programming/src/org/howard/edu/lsp/assignment2/Driver.java
@@ -7,3 +7,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Driver {
+	public static void main(String[] args) {
+		String filePath = "src/main/java/resources/word.text";
+		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+			String line;
+			Map<String, Integer> wordCounts = new HashMap<>();
+			while ((line = br.readLine()) != null) {
+				String[] words = line.split(" ");
+				for (String word : words) {
+					word = word.toLowerCase();
+					if (word.length() <= 3) {
+						continue;
+					}
