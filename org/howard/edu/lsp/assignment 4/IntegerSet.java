7ee6c5e811c8e39b5b86bc1fc3e4a13beb77996e
Morgan Thornton <morgan.thornton@bison.howard.edu>
2023-02-14 23:57:21 -0500

    Update IntegerSet.java

--- org/howard/edu/lsp/assignment 4/IntegerSet.java
+++ org/howard/edu/lsp/assignment 4/IntegerSet.java
@@ -7,3 +7,13 @@ import java.util.List;
  * This class implements the functionality of Integer Set
  * @author Morgan Thornton
  */
+
+public class IntegerSet  {
+	// Hint: probably best to use an array list.  You will need to do a little research
+	private List<Integer> set = new ArrayList<Integer>();
+
+	// Default Constructor
+	public IntegerSet() {
+	}
+
+	/**
