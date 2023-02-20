4027c9839c0dc28db8b8ce14d39a499b6d9d26aa
Morgan Thornton <morgan.thornton@bison.howard.edu>
2023-02-20 00:07:50 -0500

    Update Driver.java

--- org/howard/edu/lsp/assignment 4/Driver.java
+++ org/howard/edu/lsp/assignment 4/Driver.java
@@ -8,3 +8,14 @@ public class Driver {
         IntegerSet set1 = new IntegerSet();
         set1.add(1);
         set1.add(2);
+     	set1.add(3);
+
+        System.out.println("Value of Set1 is:" + set1.toString());
+        System.out.println("Smallest value in Set1 is:" + set1.smallest());
+        System.out.println("Largest value in Set1 is:" + set1.largest());
+        
+        IntegerSet set2 = new IntegerSet();
+        set2.add(4);
+        set2.add(5);
+
+ 
