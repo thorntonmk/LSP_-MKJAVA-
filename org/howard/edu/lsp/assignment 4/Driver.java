451282ddd9c5cdb02f08a7aaa4755750112c0868
Morgan Thornton <morgan.thornton@bison.howard.edu>
2023-02-20 00:07:06 -0500

    Create Driver.java

---
+++ org/howard/edu/lsp/assignment 4/Driver.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.assignment4;
+/**
+ * This class tests the implementation of IntegerSet.
+ * @author Morgan Thornton
+ */
+public class Driver {
+    public static void main(String args[]) throws IntegerSetException{
+        IntegerSet set1 = new IntegerSet();
+        set1.add(1);
+        set1.add(2);
