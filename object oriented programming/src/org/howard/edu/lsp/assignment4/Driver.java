1479885226270c37663d1af43bc66cbb7cb9b8b2
Morgan Thornton <morgan.thornton@bison.howard.edu>
2023-02-27 21:42:18 -0500

    Update Driver.java

--- object oriented programming/src/org/howard/edu/lsp/assignment4/Driver.java
+++ object oriented programming/src/org/howard/edu/lsp/assignment4/Driver.java
@@ -17,3 +17,17 @@ public class Driver {
         IntegerSet set2 = new IntegerSet();
         set2.add(4);
         set2.add(5);
+
+        System.out.println("Union of Set1 and Set2");
+        System.out.println("Value of Set1 is:" + set1.toString());
+        System.out.println("Value of Set2 is:" + set2.toString());
+        set1.union(set2);	// union of set1 and set2
+        System.out.println("Result of union of Set1 and Set2");
+        System.out.println(set1.toString());	// result of union of set1 and set2
+        
+        
+        set1.clear();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        
