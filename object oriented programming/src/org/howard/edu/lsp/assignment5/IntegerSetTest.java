a6632dc2a371b1a6236d75e0cc174a127d6a759e
Morgan Thornton <morgan.thornton@bison.howard.edu>
2023-03-10 14:05:29 -0500

    Update IntegerSetTest.java

---
+++ object oriented programming/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment5;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+
+/**
+ *
+ * @author PC
+ */
+public class IntegerSetTest {
+    
+    public IntegerSetTest() {
+    }
+
+    /**
+     * Test of clear method, of class IntegerSet.
