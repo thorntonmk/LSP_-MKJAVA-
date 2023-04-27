f716fbdf1bbb214bb7cf6ebbd33328db8e986e27
Morgan Thornton <morgan.thornton@bison.howard.edu>
2023-04-27 19:51:15 -0400

    Update SongsDatabase.java
     Key is the genre, HashSet contains associated songs

--- object oriented programming/src/org/howard/edu/lsp/finalsycs363/problem/SongsDatabase.java
+++ object oriented programming/src/org/howard/edu/lsp/finalsycs363/problem/SongsDatabase.java
@@ -10,3 +10,16 @@ import java.util.Set;
  * @author Morgan Thornton
  */
 public class SongsDatabase {
+
+/* Key is the genre, HashSet contains associated songs */
+    private Map<String, HashSet<String>> map = new HashMap<String, HashSet<String>>();
+
+    /* Add a song title to a genre */
+    public void addSong(String genre, String songTitle) {
+        HashSet<String> songsSet = (HashSet)getSongs(genre);
+        if(songsSet==null){
+            songsSet = new HashSet<>();
+        }
+        songsSet.add(songTitle);
+        map.put(genre, songsSet);
+    }
